5be368a822c6986906fdc06630f5a6ac98b67743
炒饭 <chensp@59store.com>
2016-09-19 11:51:50 +0800

    add:HessianStreamResult 类

---
+++ src/Hessian/HessianStreamResult.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LibHessian\Hessian;
+
+
+include_once __DIR__ . '/HessianPHP_v2.0.3/src/HessianParsing.php';
+
+
+/**
+* 继承 HessianStreamResult,做为一个连接
+*/
+class HessianStreamResult extends \HessianStreamResult
+{
+
+
+}
